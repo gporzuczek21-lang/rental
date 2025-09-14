ed11efdbc5c50c2b4d1b9496e9d2bc015be1dd3a
Gabriela Porzuczek <gporzuczek21@gmail.com>
2025-09-14 21:20:12 +0200

    second send

--- main.py
+++ main.py
@@ -11,18 +11,10 @@ class Movis:
         self.plays = 0
 
     def __str__(self):
-        return f"{self.title} {self.yers}"
+        return f"{self.title} {self.yers} {self.plays}"
     
     def play(self):
        self.plays += 1
-       return self.plays
-    
-    def search(self):
-        look_for=input("Podaj tytuł: ")
-        if look_for in self.title:
-            print(look_for)
-        else:
-            print("nie znaleziono")
     
 class Series(Movis):
     def __init__(self, season, episode, *args, **kwargs):
@@ -31,7 +23,7 @@ class Series(Movis):
         self.episode = episode
 
     def __str__(self):
-        return f"{self.title} S0{self.season}E0{self.episode}"
+        return f"{self.title} S0{self.season}E0{self.episode} {self.plays}"
         
 def creator_library(how_many):
     library = []
@@ -43,32 +35,36 @@ def creator_library(how_many):
         library.append(i)    
     return library
 
-def get_movies():
+def get_movies(rental):
     filmy = [s for s in rental if not isinstance(s,Series)]
     filmy.sort(key = lambda Movies:Movies.title)
     return filmy            
     
-def get_series():
+def get_series(rental):
     seriale = [s for s in rental if isinstance(s, Series)]   
     seriale.sort(key = lambda Series:Series.title)
     return seriale
     
-def generate_views():
+def generate_views(rental):
     lotto = random.choice(rental)
     los = random.randint(1, 100)
     lotto.plays += los
     
-def generate_views10():
+def generate_views10(rental):
     for i in range(10):
-         generate_views()
+        generate_views(rental)   
 
-def top_titles():
+def top_titles(rental):
+    how = int(input("Podaj ile tytułów mam wyświetlić: "))
     top = sorted(rental, key=lambda rental:rental.plays, reverse=True)
-    return top
+    najj = (top[:how])
+    for n in najj:
+        print(n)
 
-def search(element):
-    (obj for obj in rental if obj.title == element)
-    return element
+def search(rental):
+    element = str(input("podaj szykany tytuł: "))
+    szukana = (obj for obj in rental if obj.title == element)
+    return szukana 
 
 if __name__=='__main__':
     today = date.today()
@@ -77,20 +73,11 @@ if __name__=='__main__':
     for r in rental:
         print(r)
 
-    generate_views10()
+    generate_views10(rental)
+    for r in rental:
+        print(r)
+   
     print(f"Najpopularniejsze filmy i seriale dnia {today.day}.{today.month}.{today.year}")
-    how = int(input("Podaj ile tytułów mam wyświetlić: "))
-    topp = top_titles()
-    najj= (topp[:how])
-    for n in najj:
-        print(n)   
-
-    element = str(input("podaj szykany tytuł: "))
-    print(search(element))
-    
-    
-    
-    
-    
-
-
+    top_titles(rental)
+   
+    print(search(rental))
